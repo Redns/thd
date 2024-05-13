d3f69f47d4e37e5ce88c2dd770af5de525c66fbb
Redns <ljing431@163.com>
2024-05-13 16:52:48 +0800

    init repo

---
+++ doc/note.md
@@ -0,0 +1,7 @@
+# 1 串口无输出/输出乱码、HAL_Delay 卡顿
+
+Proteus 中的振荡器频率对应 `SYSCLK`
+
+# 2 ADC 采集电压始终为 0
+
+单片机 VDDA 和 VSSA 不能悬空
---
+++ src/Inc/fft.h
@@ -0,0 +1,18 @@
+#ifndef __FFT_H__
+#define __FFT_H__
+
+#define FFT_N 128                                        //定义傅里叶变换的点数
+#define PI 3.14159265358979323846264338327950288419717  //定义圆周率值
+
+struct compx { double real, imag; };                    //定义一个复数结构
+
+extern struct compx Compx[];							//FFT输入和输出：从Compx[0]开始存放，根据大小自己定义
+extern double SIN_TAB[];								//正弦信号表
+
+void Refresh_Data(struct compx *xin, int id, double wave_data);
+void create_sin_tab(double *sin_t);
+void FFT(struct compx *xin);
+void Get_Result(struct compx *xin, double sample_frequency);
+
+#endif
+
