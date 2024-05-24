7f818b8685c2c6965179c62fa35c8408b89c1b40
Redns <ljing431@163.com>
2024-05-24 14:36:56 +0800

    ✨ 新增串口检测功能

---
+++ src/thd.wave_viewer/WaveViewer/Components/Pages/Home.razor.cs
@@ -0,0 +1,23 @@
+﻿
+using System.IO.Ports;
+
+namespace WaveViewer.Components.Pages
+{
+    partial class Home
+    {
+        private string[]? _serialPorts;
+
+        protected override async Task OnInitializedAsync()
+        {
+            RefreshSerialPorts();
+            await base.OnInitializedAsync();
+        }
+
+        private void RefreshSerialPorts()
+        {
+#if WINDOWS
+            _serialPorts = SerialPort.GetPortNames();
+#endif
+        }
+    }
+}
